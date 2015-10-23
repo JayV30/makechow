fe21c4bcf2756eff30d3db0a746b5d4a33d7bdfa
Jason Karlsven <j.karlsven@gmail.com>
2015-10-23 20:22:17 +0000

    Add User Profile Page and User Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'static_pages#home'
+  
+  get 'signup' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  # root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
