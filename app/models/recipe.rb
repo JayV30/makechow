d124db6d859ccdbafcafa8725b89142753dbf05d
Jason Karlsven <j.karlsven@gmail.com>
2015-11-11 22:37:25 +0000

    More changes to Recipe.search

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,7 +18,7 @@ class Recipe < ActiveRecord::Base
   def self.search(value)
     if value
       if Rails.env.production?
-        where(['title ILIKE :search_term OR description ILIKE :search_term OR id ILIKE :search_term', search_term: "%" + value + "%"])
+        where(['title ILIKE :search_term OR description ILIKE :search_term', search_term: "%" + value + "%"])
       else
         where(['title LIKE :search_term OR description LIKE :search_term OR id LIKE :search_term', search_term: "%" + value + "%"])
       end
