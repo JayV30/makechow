e4356b00223d5f9b38949a6c86dbf943890eda45
Jason Karlsven <j.karlsven@gmail.com>
2015-11-21 18:33:09 +0000

    Add review model tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,6 @@ class Review < ActiveRecord::Base
   
   default_scope -> { order(created_at: :desc) }
   
-  validates :user_id, presence: true
   validates :recipe_id, presence: true
   validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   validates :content, allow_blank: true, length: { maximum: 1000 }
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @review = reviews(:review_one)
+  end
+  
+  test "review should be valid" do
+    assert @review.valid?
+  end
+  
+  test "content may be blank" do
+    @review.content = "  "
+    assert @review.valid?
+  end
+  
+  test "rating should be present" do
+    @review.rating = nil
+    assert_not @review.valid?
+  end
+  
+  test "recipe_id should be present" do
+    @review.recipe_id = nil
+    assert_not @review.valid?
+  end
+  
+  test "content should be less than 1000 characters" do
+    @review.content = "x" * 1001
+    assert_not @review.valid?
+  end
+  
+  test "rating should be an integer from 1 to 5 (inclusive)" do
+    assert_kind_of Integer, @review.rating
+    @review.rating = 0
+    assert_not @review.valid?
+    @review.rating = 6
+    assert_not @review.valid?
+    @review.rating = 5
+    assert @review.valid?
+  end
+  
+  test "order should be most recent first" do
+    assert_equal reviews(:review_three), Review.first
+  end
 end
